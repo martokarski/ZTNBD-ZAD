a74e1d9c24e268a4bedc5ae95a16ee4f5a148fe1
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-11-16 19:43:59 +0100

    Fix run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,12 @@
 if [[ -z "$1" ]]; then
 	echo "Syntax:"
-	echo "run <server_user>"
+	echo "run <server_user> [silent]"
 	exit
+fi
+
+SPARK_STREAM=
+if [ "$2" == "silent" ]; then
+    SPARK_STREAM="2>/dev/null"
 fi
 
 # Configuration variables
@@ -14,14 +19,17 @@ REL_DIR="`dirname \"$0\"`/.."
 ABS_DIR="`( cd \"$REL_DIR\" && pwd )`"
 SERVER_ADDR=$SERVER_USER@$SERVER_HOST
 
-ssh $SERVER_ADDR "mkdir ~/ztnbd 2>/dev/null"
+ssh $SERVER_ADDR "rm -rf ~/ztnbd && mkdir ~/ztnbd 2>/dev/null"
 scp "$ABS_DIR/Pipfile" "$SERVER_ADDR:~/ztnbd/Pipfile"
 scp "$ABS_DIR/Pipfile.lock" "$SERVER_ADDR:~/ztnbd/Pipfile.lock"
 scp -r "$ABS_DIR/src" "$SERVER_ADDR:~/ztnbd/src"
+scp -r "$ABS_DIR/resources" "$SERVER_ADDR:~/ztnbd/resources"
 
 ssh $SERVER_ADDR << SSH_SESS
 	cd ztnbd
 	pipenv install
-	#hdfs dfs -copyFromLocal src/pages/page1.html /input/page1.html
-	pipenv run spark-submit "src/$MAIN_SCRIPT"
+	hdfs dfs -mkdir -p /user/TZ/$SERVER_USER/ztnbd
+	hdfs dfs -copyFromLocal -f resources/pages/*.html /user/TZ/$SERVER_USER/ztnbd/
+	echo "===================== $MAIN_SCRIPT ====================="
+	pipenv run spark-submit "src/$MAIN_SCRIPT" $SERVER_USER $SPARK_STREAM
 SSH_SESS
--- src/MLPipeline.py
+++ src/MLPipeline.py
@@ -11,6 +11,7 @@ from pyspark.sql.functions import *
 from bs4 import BeautifulSoup
 from pyspark.ml.feature import HashingTF, Tokenizer, IDF, VectorAssembler
 from pyspark.ml.classification import DecisionTreeClassifier
+import sys
 
 
 # In[2]:
@@ -30,7 +31,8 @@ sess = sql.SparkSession(sc)
 
 labels = [1, 2, 2]
 set_label = udf(lambda i: labels[i], IntegerType())
-df = sc.wholeTextFiles('pages/*') .map(lambda row: (row[0], row[1].replace('\n', ''))) .toDF(['file', 'content']).withColumn('label', set_label(monotonically_increasing_id()))
+username = sys.argv[1]
+df = sc.wholeTextFiles("hdfs:///user/TZ/%s/ztnbd/*" % (username)) .map(lambda row: (row[0], row[1].replace('\n', ''))) .toDF(['file', 'content']).withColumn('label', set_label(monotonically_increasing_id()))
 
 
 # In[5]:
@@ -47,14 +49,14 @@ from pyspark.ml.param.shared import *
 from pyspark.ml import Transformer, Pipeline
 
 class PageSplitterTransformer(Transformer, HasInputCol):
-    def __init__(self):
-        super().__init__()
+    # def __init__(self):
+    #     super().__init__()
 
     def _transform(self, dataset):
         def split_page(row):
             html = row[1]
             parser = BeautifulSoup(html, 'html5lib')
-            return (row[0], [t.name for t in parser.body.find_all()], parser.body.getText(), str(parser.title.getText()))
+            return (row[0], [t.name for t in parser.body.find_all()], parser.body.getText(), (parser.title.getText()).encode('utf-8').strip())
         
         id_col = str(uuid.uuid1())
         in_col = self.getInputCol()
@@ -104,13 +106,13 @@ pipeline.fit(df).transform(df).select('label', 'prediction').show(truncate=False
 # In[16]:
 
 
-invalid_pipeline = Pipeline(stages=[splitter, body_hasher, body_tokenizer])
+# invalid_pipeline = Pipeline(stages=[splitter, body_hasher, body_tokenizer])
 
 
 # In[17]:
 
 
-invalid_pipeline.fit(df).transform(df)
+# invalid_pipeline.fit(df).transform(df)
 
 
 # In[ ]:
