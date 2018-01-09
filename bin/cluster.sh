45e9e7bb648f6be2afeffdf34d5414555b3bc2ad
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2018-01-09 14:51:54 +0100

    Run notebook on cluster

---
+++ bin/cluster.sh
@@ -0,0 +1,37 @@
+if [[ -z "$1" ]]; then
+	echo "Syntax:"
+	echo "run <server_user> [silent]"
+	exit
+fi
+
+SPARK_STREAM=
+if [ "$2" == "silent" ]; then
+    SPARK_STREAM="2>/dev/null"
+fi
+
+# Configuration variables
+MAIN_SCRIPT="PipelineCV"
+SERVER_USER=$1
+SERVER_HOST=153.19.52.196
+
+# Script variables
+REL_DIR="`dirname \"$0\"`/.."
+ABS_DIR="`( cd \"$REL_DIR\" && pwd )`"
+SERVER_ADDR=$SERVER_USER@$SERVER_HOST
+
+ssh $SERVER_ADDR "rm -rf ~/ztnbd && mkdir ~/ztnbd 2>/dev/null"
+scp "$ABS_DIR/$MAIN_SCRIPT.ipynb" "$SERVER_ADDR:~/ztnbd/$MAIN_SCRIPT.ipynb"
+scp "$ABS_DIR/Pipfile" "$SERVER_ADDR:~/ztnbd/Pipfile"
+scp "$ABS_DIR/Pipfile.lock" "$SERVER_ADDR:~/ztnbd/Pipfile.lock"
+scp -r "$ABS_DIR/external" "$SERVER_ADDR:~/ztnbd/external"
+scp -r "$ABS_DIR/post_extractor" "$SERVER_ADDR:~/ztnbd/post_extractor"
+
+ssh $SERVER_ADDR << SSH_SESS
+	cd ztnbd
+	pipenv --python 3.6.4 install
+
+    jupyter nbconvert --to script $MAIN_SCRIPT.ipynb
+
+	echo "===================== $MAIN_SCRIPT.py ====================="
+	pipenv --python 3.6.4 run spark-submit --conf spark.ui.enabled=true $MAIN_SCRIPT.py $SERVER_USER $SPARK_STREAM]
+SSH_SESS
