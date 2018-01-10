3451d83c2456e223ba009978d6d9711793039769
marracz <martaraczynska94@op.pl>
2018-01-10 22:53:00 +0100

    Adding nested params

---
+++ post_extractor/modules/features_.py
@@ -0,0 +1,211 @@
+# coding=utf-8
+import json
+import statistics
+
+from abc import ABC, abstractmethod
+
+from pyspark import keyword_only
+from pyspark.ml import Transformer
+from pyspark.ml.param.shared import (
+    HasInputCol, HasOutputCol, Param
+)
+from pyspark.sql.functions import udf
+from pyspark.sql.types import (
+    ArrayType, StringType
+)
+
+
+class BaseFeatureTransformer(Transformer, HasInputCol, HasOutputCol, ABC):
+
+    @keyword_only
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.features = Param(self, "features", "")
+        self._setDefault(features=set())
+        self.set_params(**self._input_kwargs)
+
+    @keyword_only
+    def set_params(self, **kwargs):
+        return self._set(**self._input_kwargs)
+
+    def set_features(self, value):
+        self._paramMap[self.features] = value
+        return self
+
+    def get_features(self):
+        return self.getOrDefault(self.features)
+
+    @abstractmethod
+    def _transform(self, dataframe):
+        pass
+
+
+class MaxFeaturesTransformer(BaseFeatureTransformer):
+    """
+    Klasa MaxFeaturesTransformer dziedziczy  po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Klasa ta przyjmuje dodatkowy parametr features, który zawiera listę nazw cech,
+    które mają zostać zagregowane. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe. Metoda
+    ta wylicza średnie wartości dla cech podanych w parametrze features, ze wszystkich obiektów jsonowych znajdujących
+    się w tekście zawartym w kolumnie inputCol. Wyliczone wartości wstawia do kolumny outputCol w postaci listy
+    double’i.
+    """
+    def _transform(self, dataframe):
+
+        features = self.get_features()
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def feature_collect(data):
+            features_dict = {}
+
+            for feature in features:
+                features_dict[feature] = 0
+            lines = data.splitlines(keepends=False)
+
+            for line in lines:
+                json_line = json.loads(line)
+                feature_array = json_line.get('features')
+
+                for element in feature_array:
+                    name = element.get('name')
+                    if name in features_dict:
+                        features_dict[name] = max(features_dict[name],element.get('value'))
+
+            values = []
+            for feature in features:
+                values.append(features_dict[feature])
+            return values
+
+        get_cntn = udf(feature_collect, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, get_cntn(in_col))
+
+
+class MeanFeaturesTransformer(BaseFeatureTransformer):
+    """
+    Klasa MeanFeaturesTransformer dziedziczy  po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Klasa ta przyjmuje dodatkowy parametr features, który zawiera listę nazw cech,
+    które mają zostać zagregowane. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe. Metoda
+    ta wylicza średnie wartości dla cech podanych w parametrze features, ze wszystkich obiektów jsonowych znajdujących
+    się w tekście zawartym w kolumnie inputCol. Wyliczone wartości wstawia do kolumny outputCol w postaci listy
+    double’i.
+    """
+    def _transform(self, dataframe):
+
+        features = self.get_features()
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def feature_collect(data):
+            features_dict = {}
+            count_dict = {}
+
+            for feature in features:
+                features_dict[feature] = 0
+                count_dict[feature] = 0
+            lines = data.splitlines(keepends=False)
+
+            for line in lines:
+                json_line = json.loads(line)
+                feature_array = json_line.get('features')
+
+                for element in feature_array:
+                    name = element.get('name')
+                    if name in features_dict:
+                        features_dict[name] += element.get('value')
+                        count_dict[name] += 1
+
+            values = []
+            for feature in features:
+                if count_dict[feature] != 0:
+                    values.append(features_dict[feature] / count_dict[feature])
+                else:
+                    values.append(0)
+            return values
+
+        get_cntn = udf(feature_collect, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, get_cntn(in_col))
+
+
+class MedianFeaturesTransformer(BaseFeatureTransformer):
+    """
+    Klasa MedianFeaturesTransformer dziedziczy  po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Klasa ta przyjmuje dodatkowy parametr features, który zawiera listę nazw cech,
+    które mają zostać zagregowane. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe. Metoda
+    ta wylicza medianę dla cech podanych w parametrze features, ze wszystkich obiektów jsonowych znajdujących się
+    w tekście zawartym w kolumnie inputCol. Wyliczone wartości wstawia do kolumny outputCol w postaci listy double’i.
+    """
+    def _transform(self, dataframe):
+
+        features = self.get_features()
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def feature_collect(data):
+            features_dict = {}
+            count = 0
+
+            for feature in features:
+                features_dict[feature] = []
+            lines = data.splitlines(keepends=False)
+
+            for line in lines:
+                count += 1
+                json_line = json.loads(line)
+                feature_array = json_line.get('features')
+
+                for element in feature_array:
+                    name = element.get('name')
+                    if name in features_dict:
+                        features_dict[name].append(element.get('value'))
+
+            values = []
+            for feature in features:
+                if not features_dict[feature]:
+                    values.append(0)
+                else:
+                    values.append(statistics.median(features_dict[feature]))
+            return values
+
+        get_cntn = udf(feature_collect, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, get_cntn(in_col))
+
+
+class NumberOfOccurrencesFeaturesTransformer(BaseFeatureTransformer):
+    """
+    Klasa MedianFeaturesTransformer dziedziczy  po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Klasa ta przyjmuje dodatkowy parametr features, który zawiera listę nazw cech,
+    które mają zostać zagregowane. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe. Metoda
+    ta wylicza medianę dla cech podanych w parametrze features, ze wszystkich obiektów jsonowych znajdujących się
+    w tekście zawartym w kolumnie inputCol. Wyliczone wartości wstawia do kolumny outputCol w postaci listy double’i.
+    """
+    def _transform(self, dataframe):
+
+        features = self.get_features()
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def feature_collect(data):
+            features_dict = {}
+            count = 0
+
+            for feature in features:
+                features_dict[feature] = 0
+            lines = data.splitlines(keepends=False)
+
+            for line in lines:
+                count += 1
+                json_line = json.loads(line)
+                feature_array = json_line.get('features')
+
+                for element in feature_array:
+                    name = element.get('name')
+                    if name in features_dict:
+                        features_dict[name] += 1
+
+            values = []
+            for feature in features:
+                values.append(features_dict[feature])
+            return values
+
+        get_cntn = udf(feature_collect, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, get_cntn(in_col))
---
+++ post_extractor/modules/posts.py
@@ -0,0 +1,147 @@
+# coding=utf-8
+import json
+import ast
+
+from textblob import TextBlob
+from textblob.exceptions import NotTranslated, TranslatorError
+
+from pyspark.ml import Transformer
+from pyspark.ml.param.shared import HasInputCol, HasOutputCol
+from pyspark.sql.functions import udf
+from pyspark.sql.types import ArrayType, StringType, MapType, IntegerType, StructType, StructField, DoubleType
+
+
+class PostTransformer(Transformer, HasInputCol, HasOutputCol):
+    """
+    Klasa PostTransformer dziedziczy po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe.
+    Metoda ta wydobywa z kolumny inputCol, z formatu json, content i umieszcza go w kolumnie outputCol w postaci tekstu.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def _transform(self, dataframe):
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def get_content(data):
+            contents = [doc.get('content') for doc in data]
+            return contents
+
+        get_cntn = udf(get_content, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, get_cntn(in_col))
+
+
+class TranslateTransformer(Transformer, HasInputCol, HasOutputCol):
+    """
+    Klasa TranslateTransformer dziedziczy po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe.
+    Metoda ta tłumaczy tekst zawarty w kolumnie inputCol  z języka polskiego na angielski i umieszcza go w kolumnie
+    outputCol.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def _transform(self, dataframe):
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def translate(p):
+            try:
+                return str(TextBlob(p).translate(from_lang='pl'))
+            except (NotTranslated, TranslatorError) as e:
+                return 'Translation error'
+
+        def process(data):
+            translated = [translate(p) for p in data]
+            return translated
+
+        process = udf(process, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, process(in_col))
+
+
+class SentenceTransformer(Transformer, HasInputCol, HasOutputCol):
+    """
+    Klasa SentenceTransformer dziedziczy po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe.
+    Metoda ta dzieli tekst zawarty w kolumnie inputCol  na zdania i umieszcza go w kolumnie outputCol w postaci
+    tablicy tekstów.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def _transform(self, dataframe):
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def extract_sentences(data):
+            sentences = []
+            for post in data:
+                for sentence in TextBlob(post).sentences:
+                    sentences.append(str(sentence))
+            return sentences
+
+        ext_sentn = udf(extract_sentences, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, ext_sentn(in_col))
+
+class SpeechPartsTransformer(Transformer, HasInputCol, HasOutputCol):
+    """
+    Klasa SpeechPartsTransformer dziedziczy  po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe.
+    Metoda ta z tekstu zawartego w kolumnie inputCol  zlicza wystąpienie części mowy i wstawia do outputCol w postaci
+    jsona.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def _transform(self, dataframe):
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def tags_sum_by_key(tags):
+            types = {}
+
+            for tag in tags:
+                if tag[1] in types:
+                    types[tag[1]] += 1
+                else:
+                    types[tag[1]] = 1
+
+            return types
+        
+        def extract_speech_parts(data):
+            tags = []
+            for post in data:
+                tags.extend(TextBlob(post).tags)
+            speech_parts = tags_sum_by_key(tags)
+            return speech_parts
+
+        ext_speech_parts = udf(extract_speech_parts, MapType(StringType(), IntegerType()))
+        return dataframe.withColumn(out_col, ext_speech_parts(in_col))
+
+class SentimentTransformer(Transformer, HasInputCol, HasOutputCol):
+    """
+    Klasa SentimentTransformer dziedziczy  po klasach pyspark.ml.Transformer, pyspark.ml.param.shared.HasInputCol,
+    pyspark.ml.param.shared.HasOutputCol. Posiada metodę transform, która przyjmuje na wejściu obiekt typu dataframe.
+    Metoda ta z tekstu zawartego w kolumnie inputCol  wylicza sentiment i wstawia do kolumny outputCol.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def _transform(self, dataframe):
+        out_col = self.getOutputCol()
+        in_col = self.getInputCol()
+
+        def calculate_sentiment(data):
+            sentiments = []
+            for post in data:
+                sentiments.append(TextBlob(post).sentiment)
+            return sentiments
+
+        cnt_senti = udf(calculate_sentiment, ArrayType(
+            StructType(
+                [StructField("polarity", DoubleType()),
+                 StructField("subjectivity", DoubleType())])
+            )
+        )
+        return dataframe.withColumn(out_col, cnt_senti(in_col))
